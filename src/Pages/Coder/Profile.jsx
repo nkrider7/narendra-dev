2354135d6e5869b9994f8a9ea0785e3bb604fed1
narendra <narendranishad59@gmail.com>
2024-08-10 10:24:17 +0530

    chore: Update social media links, project section description, and add loader

--- src/Pages/Coder/Profile.jsx
+++ src/Pages/Coder/Profile.jsx
@@ -6,6 +6,7 @@ import {
   BsInstagram,
 } from "react-icons/bs";
 import bg from "../../assets/bg.gif"
+import gif from "./OG.gif";
 
 
 export default function Profile() {
@@ -44,15 +45,46 @@ export default function Profile() {
       logo: <BsFacebook size={40} />,
     },
   ];
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/resume.pdf"; // Directly reference the file path
+    link.download = "NarendraFullStackResume.pdf"; // Set the file name for the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div>
-      <div style={{backgroundImage: `url(${bg})`, backgroundSize:250,backgroundBlendMode:"hue"}} className="flex justify-center   bg-no-repeat bg-center bg-blend-hue bg-contain items-center mt-20 flex-col h-fit border-b-2 pb-5 border-dashed">
+      <div style={{backgroundImage: `url(${gif})`, backgroundSize:250,backgroundBlendMode:"hue"}} className="flex justify-center   bg-no-repeat bg-left bg-blend-hue bg-cover items-center mt-20 flex-col h-fit border-b-2 pb-5 border-dashed">
         <h1 className="text-white font-gil text-3xl md:text-5xl    text-start  ">
           Hello, I'm Narendra!
         </h1>
         <h1 className="text-white font-gil text-4xl md:text-7xl   text-start  ">
           Full <span className="text-[#37ff8b]">Stack</span> Develpoer
         </h1>
+        <div>
+       
+<button
+  onClick={handleDownload}
+  className="overflow-hidden border font-gil border-white w-32 p-2 h-12 bg-black text-white  rounded-md text-xl font-bold cursor-pointer relative z-10 group"
+>
+  Resume!
+  <span
+    className="absolute w-36 h-32 -top-8 -left-2 bg-green-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-bottom"
+  ></span>
+  <span
+    className="absolute w-36 h-32 -top-8 -left-2 bg-green-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-bottom"
+  ></span>
+  <span
+    className="absolute w-36 h-32 -top-8 -left-2 bg-green-600 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-bottom"
+  ></span>
+  <span
+    className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10"
+    >Explore!</span
+  >
+</button>
+
+        </div>
         <div className="flex gap-x-3 mt-4 justify-center flex-wrap">
           {links.map((item, index) => (
             <a
--- src/Pages/Coder/SkillsBox.jsx
+++ src/Pages/Coder/SkillsBox.jsx
@@ -1,17 +1,44 @@
-export default function SkillsBox() { 
-    return (
-      <>
-        <div className="main flex justify-center items-center flex-col md:flex-row h-fit py-20 gap-2">
-          <div className="h-60 w-80 bg-sky-100 rounded-xl text-[4rem] hover:bg-sky-600 transition cursor-pointer  font-gil text-black flex justify-center items-center">React</div>
-          <div className="flex flex-row md:flex-col gap-2">
-            <div   className="h-28 w-40   border-4  rounded-xl font-gil text-[1.5rem] hover:bg-yellow-400 hover:text-black transition text-yellow-400 flex justify-center items-center">Javascript</div>
-            <div className="h-28 w-40 bg-red-600 rounded-xl font-gil text-[9rem] text-black  overflow-clip hover:text-[2rem] transition flex justify-center items-center ">C++</div>
+import { skills } from "../../DB/skillsIcons";
+export default function SkillsBox() {
+  return (
+    <>
+     <div className="flex justify-center pt-5 flex-wrap gap-x-2">
+        { skills.map((item, index) =>(
+          <div key={index} className="group relative">
+          <button>
+            {item.icon}
+          </button>
+          <span
+            className="absolute text-black -top-14 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 px-3 rounded-lg border border-gray-300 bg-white py-2 text-sm font-bold shadow-md transition-all duration-300 ease-in-out  group-hover:scale-100">
+            {item.name}<span></span>
+          </span>
+        </div>
+        ))}
+      </div>
+      <div className="main flex justify-center items-center flex-col md:flex-row h-fit pt-5 pb-8 gap-2">
+        <div className="h-60 w-80 bg-sky-100 rounded-xl text-[4rem] hover:bg-sky-600 transition cursor-pointer  font-gil text-black flex justify-center items-center">
+          React
+        </div>
+        <div className="flex flex-row md:flex-col gap-2">
+          <div className="h-28 w-40   border-4  rounded-xl font-gil text-[1.5rem] hover:bg-yellow-400 hover:text-black transition text-yellow-400 flex justify-center items-center">
+            Javascript
           </div>
-          <div className="flex flex-row md:flex-col gap-2">
-            <div className="h-28 w-40 bg-[#37ff8b] rounded-xl font-gil transition hover:rotate-45 text-[1.5rem] text-black flex justify-center items-center "> Python</div>
-            <div className="h-28 w-40 border-blue-400 border-4 rounded-xl font-gil text-[1.5rem] text-blue-400 flex justify-center items-center "> TailwindCSS</div>
+          <div className="h-28 w-40 bg-red-600 rounded-xl font-gil text-[9rem] text-black  overflow-clip hover:text-[2rem] transition flex justify-center items-center ">
+            C++
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="flex flex-row md:flex-col gap-2">
+          <div className="h-28 w-40 bg-[#37ff8b] rounded-xl font-gil transition  text-[1.5rem] text-black flex justify-center items-center ">
+            {" "}
+            Python
+          </div>
+          <div className="h-28 w-40 border-blue-400 border-4 rounded-xl font-gil text-[1.5rem] hover:bg-slate-50 text-blue-400 flex justify-center items-center ">
+            {" "}
+            TailwindCSS
+          </div>
+        </div>
+      </div>
+     
+    </>
+  );
+}
